4100c522fabaa615a7394196e2f9c4072729960a
Marcus Ortiz <mportiz08@gmail.com>
2011-11-28 20:43:22 -0800

    implemented checkForTables()

--- src/lab7/InnDB.java
+++ src/lab7/InnDB.java
@@ -47,7 +47,7 @@ public class InnDB
       System.exit(1);
     }
     
-    checkForTable();
+    checkForTables();
   }
   
   public Connection getConnection()
@@ -55,8 +55,63 @@ public class InnDB
     return this.conn;
   }
   
-  private void checkForTable()
+  private void checkForTables()
   {
-    
+    try
+    {
+      DatabaseMetaData dbm = this.conn.getMetaData();
+      
+      // Check if "Rooms" table exists in db, make it if not
+      System.out.println("Looking for \"Rooms\" table.");
+      Statement s = this.conn.createStatement();
+      ResultSet results = s.executeQuery("SELECT TABLE_NAME FROM USER_TABLES WHERE TABLE_NAME = 'ROOMS'");
+      if(!results.next())
+      {
+        System.out.println("Table \"Rooms\" not found.");
+        s.executeUpdate("CREATE TABLE Rooms(" +
+          "Id           CHAR(3) PRIMARY KEY," +
+          "Name         VARCHAR2(50) UNIQUE," +
+          "Beds         INT," +
+          "BedType      VARCHAR2(10)," +
+          "MaxOccupancy INT," +
+          "BasePrice    INT," +
+          "Decor        VARCHAR2(20)" +
+        ")");
+      }
+      else
+      {
+        System.out.println("Table \"Rooms\" found.");
+      }
+      
+      // Check if "Reservations" table exists in db, make it if not
+      System.out.println("Looking for \"Reservations\" table.");
+      results = s.executeQuery("SELECT TABLE_NAME FROM USER_TABLES WHERE TABLE_NAME = 'RESERVATIONS'");
+      if(!results.next())
+      {
+        System.out.println("Table \"Reservations\" not found.");
+        s.executeUpdate("CREATE TABLE Reservations(" +
+                          "Code      INT PRIMARY KEY," +
+                          "Room      CHAR(3)," +
+                          "CheckIn   DATE," +
+                          "CheckOut  DATE," +
+                          "Rate      FLOAT," +
+                          "LastName  VARCHAR2(25)," +
+                          "FirstName VARCHAR2(25)," +
+                          "Adults    INT CHECK(Adults >= 1)," +
+                          "Kids      INT," +
+                          "FOREIGN KEY(Room) REFERENCES Rooms," +
+                          "CONSTRAINT NoDupes UNIQUE(Room, CheckIn)" +
+                        ")");
+      }
+      else
+      {
+        System.out.println("Table \"Reservations\" found.");
+      }
+    }
+    catch(SQLException e)
+    {
+      System.err.println("SQLException occured.");
+      System.exit(1);
+    }
   }
 }
