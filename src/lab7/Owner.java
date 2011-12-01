3fcc7450ea9facfb0b17aa535533fa1913eb3a8c
Alan Yang <alanyang@sbcglobal.net>
2011-11-30 19:43:45 -0800

    Set up windows

---
+++ src/lab7/Owner.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7;
+
+import java.sql.*;
+/**
+ *
+ * @author spartan
+ */
+public class Owner {
+    
+    private Connection conn;
+    
+    public Owner(Connection c) {
+        this.conn = c;
+    }
+    
+    
+}
