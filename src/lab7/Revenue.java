6ba02ee0e587ef735c565e564a4f6b943033b5d5
Marcus Ortiz <mportiz08@gmail.com>
2011-12-02 16:00:11 -0800

    working on table models for revenue

--- src/lab7/Revenue.java
+++ src/lab7/Revenue.java
@@ -1,6 +1,7 @@
 package lab7;
 
 import java.sql.*;
+import javax.swing.table.AbstractTableModel;
 
 /**
  * Revenue model class.
@@ -8,10 +9,63 @@ import java.sql.*;
  */
 public class Revenue
 {
+  private class BaseTableModel extends AbstractTableModel
+  {
+    private Object[][] data;
+    private String[] cols;
+
+    public BaseTableModel(Object[][] data, String[] cols)
+    {
+      this.data = data;
+      this.cols = cols;
+    }
+
+    public Object getValueAt(int row, int col)
+    {
+      return this.data[row][col];
+    }
+
+    public int getRowCount()
+    {
+      return data.length;
+    }
+
+    @Override
+    public String getColumnName(int i)
+    {
+      return this.cols[i];
+    }
+
+    public int getColumnCount()
+    {
+      return this.cols.length;
+    }
+
+    @Override
+    public Class getColumnClass(int i)
+    {
+      return this.cols[i].getClass();
+    }
+  }
+  
   private Connection conn;
   
   public Revenue(Connection c)
   {
     this.conn = c;
+  }
+  
+  public AbstractTableModel getResTable()
+  {
+    String[] cols = {};
+    Object[][] data = {};
+    return new BaseTableModel(data, cols);
+  }
+  
+  public AbstractTableModel getRevTable()
+  {
+    String[] cols = {};
+    Object[][] data = {};
+    return new BaseTableModel(data, cols);
   }
 }
